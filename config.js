a8e09f76a9797014c2963169995d7ef2b2cd27f3
mehulganeshaaa-stack <mehul.ganeshaaa@gmail.com>
2026-08-26 12:51:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.BRICKROOTS_CONFIG={
   apiBase:"https://yzedjulorzxjwpwnwstq.supabase.co",
-  supabaseAnonKey:"sb_publishable_O8MuUKbNZK7n9myIZK-bjw_k_h4rM-Z",
+  supabaseAnonKey:sb_publishable_O8MuUKbNZK7n9myIZK-bjw_k_h4rM-Z,
   googleReviewUrl:"https://g.page/r/CVaTKjso2WJFEBM/review?utm_source=gbp&utm_medium=reviews&utm_campaign=qr"
 };
